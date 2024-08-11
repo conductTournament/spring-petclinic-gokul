9425f488155b8bc0899607e756812aa9a0693e2e
Gokul_repo <126143772+conductTournament@users.noreply.github.com>
2024-08-11 10:24:09 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar /home/ubuntu/app/your-application.jar > /home/ubuntu/app/application.log 2>&1 &
+nohup java -jar /home/ubuntu/app/spring-petclinic-3.3.0-SNAPSHOT.jar.jar > /home/ubuntu/app/application.log 2>&1 &
