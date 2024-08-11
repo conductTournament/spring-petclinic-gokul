7e72b6fc0e3608b9f43e525bb490c6e50f5b1ebe
Gokul_repo <126143772+conductTournament@users.noreply.github.com>
2024-08-11 10:40:17 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+aws s3 sync s3://gokul-artifact/myartifact/ /home/ec2-user/myartifact/
+mv /home/ec2-user/myartifact/target/spring-petclinic-3.3.0-SNAPSHOT.jar /home/ec2-user/app/
+sudo systemctl daemon-reload
+sudo systemctl start my-java-app.service
+sudo systemctl enable my-java-app.service
 nohup java -jar /home/ubuntu/app/spring-petclinic-3.3.0-SNAPSHOT.jar.jar > /home/ubuntu/app/application.log 2>&1 &
