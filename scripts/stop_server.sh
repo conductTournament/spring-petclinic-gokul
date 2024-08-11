6f51b36682a59044f0198c2ca44b3aeb50be4ccd
Gokul_repo <126143772+conductTournament@users.noreply.github.com>
2024-08-11 10:39:55 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 pkill -f spring-petclinic-3.3.0-SNAPSHOT.jar || true
-aws s3 sync s3://gokul-artifact/myartifact/ /home/ec2-user/myartifact/
-mv /home/ec2-user/myartifact/target/spring-petclinic-3.3.0-SNAPSHOT.jar /home/ec2-user/app/
-sudo systemctl daemon-reload
-sudo systemctl start my-java-app.service
-sudo systemctl enable my-java-app.service
+
