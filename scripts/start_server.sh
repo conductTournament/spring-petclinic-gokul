6248e58c52cbe3c26f6604e928e74581fa399910
Gokul_repo <126143772+conductTournament@users.noreply.github.com>
2024-08-13 11:16:53 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo systemctl daemon-reload
-sudo systemctl stop my-app-java.service
 sudo systemctl start my-app-java.service
 sudo systemctl enable my-app-java.service
 nohup java -jar /home/ubuntu/app/spring-petclinic-3.3.0-SNAPSHOT.jar > /home/ubuntu/app/application.log 2>&1 &
